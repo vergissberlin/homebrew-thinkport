2e252272a7d6871b3d336c5605fddd3a335c7585
André Lademann <alademann@thinkport.digital>
2023-07-04 09:17:20 +0200

    Add inter mac support

--- thinkport.rb
+++ thinkport.rb
@@ -1,14 +1,33 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+# Documentation: 
+# https://docs.brew.sh/Formula-Cookbook
+# https://rubydoc.brew.sh/Formula
+
 class Thinkport < Formula
   desc "This cli tool provides a set of commands related to Thinkport. This is a technical experiement and not intended for production use."
   homepage "https://github.com/vergissberlin/thinkport"
-  url "https://github.com/vergissberlin/thinkport/releases/download/0.0.30/thinkport_Darwin_arm64.tar.gz"
-  sha256 "d07376af1eaf40ab4d61adde4534f37e0020fbe622a635ca2e19b6cfd5c945b8"
   license "MIT"
+  depends_on "go" => :optional
 
-  # depends_on "cmake" => :build
+
+ on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/vergissberlin/thinkport/releases/download/0.0.31/thinkport_Darwin_x86_64.tar.gz"
+      sha256 "a379e371ff51b9049fe5beda8da5ec9d879964dbbd9e7c5d105a00dc1601dbdd"
+
+      def install
+        bin.install "thinkport"
+      end
+    end
+    if Hardware::CPU.arm?
+        url "https://github.com/vergissberlin/thinkport/releases/download/0.0.30/thinkport_Darwin_arm64.tar.gz"
+        sha256 "b585d78a903fc2f8a921cf8681683dea684b4ebb62cba29a4ae8f744119b02fc"
+      def install
+        bin.install "thinkport"
+      end
+    end
+  end
+
+
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
