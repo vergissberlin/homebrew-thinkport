dcf6eb9ac77a944c076e1f7b2d0b2001fdbd86f2
vergissberlin <vergissberlin@googlemail.com>
2024-11-04 16:49:18 +0100

    Brew formula update for thinkport version 0.0.62

--- thinkport.rb
+++ thinkport.rb
@@ -5,14 +5,14 @@
 class Thinkport < Formula
   desc "This cli tool provides a set of commands related to Thinkport. This is a technical experiement and not intended for production use."
   homepage "https://github.com/vergissberlin/thinkport"
-  version "0.0.61"
+  version "0.0.62"
   license "MIT"
   depends_on :linux
 
   on_arm do
     if Hardware::CPU.is_64_bit?
-      url "https://github.com/vergissberlin/thinkport/releases/download/0.0.61/thinkport_Linux_arm64.tar.gz"
-      sha256 "d489370d745647801d919d8216b750c3fdeec4a1469cdb764e139da8951f1087"
+      url "https://github.com/vergissberlin/thinkport/releases/download/0.0.62/thinkport_Linux_arm64.tar.gz"
+      sha256 "2db5155d586027dc1b539feef203c0532f07824558f4aca835b888d9d6fb0d2c"
 
       def install
         bin.install "thinkport"
